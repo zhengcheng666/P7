7bf54fc018d872edddde05210762e543ac4e0fca
Hechuang3 <2114247869@qq.com>
2020-12-15 20:13:39 +0800

    hc修改的数据

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//阿康师傅江布拉克赛诺菲
     }
 }
